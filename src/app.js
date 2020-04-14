98268b0ea0776b25d9a17aef67dbfc2e009333e7
Tamara Bain <tamara@tamaras-mbp.lan>
2020-04-14 12:03:21 +0100

    added complete button functionality

--- src/app.js
+++ src/app.js
@@ -5,20 +5,24 @@ document.addEventListener('DOMContentLoaded', () => {
     el: '#app',
     data: {
       todoItems: [
-        {name: "dance", priority: "highPrio"},
-        {name: "play", priority: "lowPrio"},
-        {name: "eat", priority: "medPrio"}],
+        {name: "dance", priority: "highPrio", status: "incomplete"},
+        {name: "play", priority: "lowPrio", status: "incomplete"},
+        {name: "eat", priority: "medPrio", status: "incomplete"}],
       newItem: "",
-      priority: "medPrio"
+      priority: "medPrio",
     },
     methods: {
       addItem: function() {
         this.todoItems.push({
           name: this.newItem,
-          priority: this.priority
+          priority: this.priority,
+          status: "incomplete"
         });
         this.newItem = "";
-        this.priority = "medPrio"
+        this.priority = "medPrio";
+      },
+      completeItem: function(index){
+        this.todoItems[index].status = 'complete';
       }
     }
   })
